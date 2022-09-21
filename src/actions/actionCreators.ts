f3833382fcd5287053cb7fbceb522c8eaa189b25
Anhelina Kuziv <anhelinakuziv@gmail.com>
2022-09-21 10:22:53 +0300

    Configured project structure

---
+++ src/actions/actionCreators.ts
@@ -0,0 +1,11 @@
+import { ADD_TODO } from './actions';
+
+// export const addTodo = (description, timestamp) => {
+//     return {
+//         type: ADD_TODO,
+//     payload: {
+//         description,
+//         timestamp,
+//     }
+//     }
+// }
---
+++ src/reducers/todoReducer.ts
@@ -0,0 +1,3 @@
+export const todoReducer = (state: any, action: any) => {
+    return state;
+};
