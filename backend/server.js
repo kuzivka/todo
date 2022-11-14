e94a98777e037223e852ac9cae5ab0b500258634
Anhelina Kuziv <anhelinakuziv@gmail.com>
2022-11-14 09:25:32 +0200

    Add server

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Server is running' });
+});
+
+app.listen(8000, () => {
+  console.log(`Server is running on port 8000.`);
+});
