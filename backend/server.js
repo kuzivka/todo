87fc5c6e0d4a1d75de97bf77fc3dd34cfceb4000
Anhelina Kuziv <anhelinakuziv@gmail.com>
2022-11-18 14:39:56 +0200

    Add catch method

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ mongoose
   )
   .then(() => {
     console.log('DB is conected');
-  });
+  }).catch((err)=>{console.log(err)})
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
